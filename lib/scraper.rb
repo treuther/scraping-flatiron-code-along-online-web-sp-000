70736cca6fc82a3d52cd5bc0a66f8af90ad594c7
Tracy Reuther <tracyreuther@gmail.com>
2019-07-16 23:39:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -30,16 +30,16 @@ class Scraper
     end
   end
 
-  #   def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course|
-  #     if course.title && course.title != ""
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-  #   end
-  # end
+    def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
 
 end
 Scraper.new.get_page
